397f1389efc184a01097af1139b7cb53afcc569a
stenkaq <vdyugay2004@gmail.com>
2026-07-19 12:27:09 +0500

    feat: add server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"go-metrics-collector/internal/handler"
+	"go-metrics-collector/internal/repository"
+	"go-metrics-collector/internal/service"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	metricsRepository := repository.NewMetricsRepository()
+	metricsService := service.NewMetricsService(metricsRepository)
+	metricsHandler := handler.NewMetricsHandler(metricsService)
+
+	mux.HandleFunc("/update/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		metricsHandler.UpdateMetric(w, r)
+	}))
+
+	http.ListenAndServe("localhost:8080", mux)
+}
