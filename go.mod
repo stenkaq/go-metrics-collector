e9f65499f6d374a5b6e5d3d1d48cc76d9d45a3e2
stenkaq <vdyugay2004@gmail.com>
2026-08-06 17:35:11 +0500

    feat: add env config

--- go.mod
+++ go.mod
@@ -3,17 +3,21 @@ module go-metrics-collector
 go 1.26.5
 
 require (
+	github.com/caarlos0/env/v11 v11.4.1
+	github.com/gin-gonic/gin v1.12.0
+	github.com/go-resty/resty/v2 v2.17.2
+)
+
+require (
 	github.com/bytedance/gopkg v0.1.4 // indirect
 	github.com/bytedance/sonic v1.15.2 // indirect
 	github.com/bytedance/sonic/loader v0.5.1 // indirect
 	github.com/cloudwego/base64x v0.1.7 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/gin-contrib/sse v1.1.1 // indirect
-	github.com/gin-gonic/gin v1.12.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.30.3 // indirect
-	github.com/go-resty/resty/v2 v2.17.2 // indirect
 	github.com/goccy/go-json v0.10.6 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,10 @@ package config
 
 import (
 	"flag"
+	"log"
 	"time"
+
+	"github.com/caarlos0/env/v11"
 )
 
 type AgentConfig struct {
@@ -11,16 +14,47 @@ type AgentConfig struct {
 	PollInterval   time.Duration
 }
 
+type AgentEnvConfig struct {
+	Address        *string `env:"ADDRESS"`
+	ReportInterval *int    `env:"REPORT_INTERVAL"`
+	PollInterval   *int    `env:"POLL_INTERVAL"`
+}
+
+type ServerEnvConfig struct {
+	Address *string `env:"ADDRESS"`
+}
 type ServerConfig struct {
 	Address string
 }
 
 func ParseAgentConfig() AgentConfig {
-	address := flag.String("a", "localhost:8080", "адрес сервера")
-	report := flag.Int("r", 10, "интервал отправки метрик (сек.)")
-	poll := flag.Int("p", 2, "интервал опроса метрик (сек.)")
+	var address *string
+	var report *int
+	var poll *int
 
+	var envCfg AgentEnvConfig
+
+	err := env.Parse(&envCfg)
+	if err != nil {
+		log.Fatalf("Ошибка конфигурации: %v", err)
+	}
+
+	address = flag.String("a", "localhost:8080", "адрес сервера")
+	report = flag.Int("r", 10, "интервал отправки метрик (сек.)")
+	poll = flag.Int("p", 2, "интервал опроса метрик (сек.)")
 	flag.Parse()
+
+	if envCfg.Address != nil {
+		address = envCfg.Address
+	}
+
+	if envCfg.PollInterval != nil {
+		poll = envCfg.PollInterval
+	}
+
+	if envCfg.ReportInterval != nil {
+		report = envCfg.ReportInterval
+	}
 
 	return AgentConfig{
 		Address:        *address,
@@ -30,9 +64,20 @@ func ParseAgentConfig() AgentConfig {
 }
 
 func ParseServerConfig() ServerConfig {
+	var envCfg ServerEnvConfig
+
 	address := flag.String("a", "localhost:8080", "адрес сервера")
 
 	flag.Parse()
+
+	err := env.Parse(&envCfg)
+	if err != nil {
+		log.Fatalf("Ошибка конфигурации: %v", err)
+	}
+
+	if envCfg.Address != nil {
+		address = envCfg.Address
+	}
 
 	return ServerConfig{
 		Address: *address,
