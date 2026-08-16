7e9404f443cc22408ab3afe514faba369f67f936
stenkaq <vdyugay2004@gmail.com>
2026-08-16 21:59:23 +0500

    refactor: env parsing

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,17 +15,18 @@ type AgentConfig struct {
 }
 
 type AgentEnvConfig struct {
-	Address        *string `env:"ADDRESS"`
-	ReportInterval *int    `env:"REPORT_INTERVAL"`
-	PollInterval   *int    `env:"POLL_INTERVAL"`
+	Address        string `env:"ADDRESS"`
+	ReportInterval int    `env:"REPORT_INTERVAL"`
+	PollInterval   int    `env:"POLL_INTERVAL"`
 }
 
 type ServerEnvConfig struct {
-	Address         *string `env:"ADDRESS"`
-	StoreInterval   *int    `env:"STORE_INTERVAL"`
-	FileStoragePath *string `env:"FILE_STORAGE_PATH"`
-	Restore         *bool   `env:"RESTORE"`
+	Address         string `env:"ADDRESS"`
+	StoreInterval   int    `env:"STORE_INTERVAL"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	Restore         bool   `env:"RESTORE"`
 }
+
 type ServerConfig struct {
 	Address         string
 	StoreInterval   time.Duration
@@ -34,73 +35,41 @@ type ServerConfig struct {
 }
 
 func ParseAgentConfig() AgentConfig {
-	var address *string
-	var report *int
-	var poll *int
+	var raw AgentEnvConfig
 
-	var envCfg AgentEnvConfig
+	flag.StringVar(&raw.Address, "a", "localhost:8080", "адрес сервера")
+	flag.IntVar(&raw.ReportInterval, "r", 10, "интервал отправки метрик (сек.)")
+	flag.IntVar(&raw.PollInterval, "p", 2, "интервал опроса метрик (сек.)")
+	flag.Parse()
 
-	err := env.Parse(&envCfg)
-	if err != nil {
+	if err := env.Parse(&raw); err != nil {
 		log.Fatalf("Ошибка конфигурации: %v", err)
 	}
 
-	address = flag.String("a", "localhost:8080", "адрес сервера")
-	report = flag.Int("r", 10, "интервал отправки метрик (сек.)")
-	poll = flag.Int("p", 2, "интервал опроса метрик (сек.)")
-	flag.Parse()
-
-	if envCfg.Address != nil {
-		address = envCfg.Address
-	}
-
-	if envCfg.PollInterval != nil {
-		poll = envCfg.PollInterval
-	}
-
-	if envCfg.ReportInterval != nil {
-		report = envCfg.ReportInterval
-	}
-
 	return AgentConfig{
-		Address:        *address,
-		ReportInterval: time.Duration(*report) * time.Second,
-		PollInterval:   time.Duration(*poll) * time.Second,
+		Address:        raw.Address,
+		ReportInterval: time.Duration(raw.ReportInterval) * time.Second,
+		PollInterval:   time.Duration(raw.PollInterval) * time.Second,
 	}
 }
 
 func ParseServerConfig() ServerConfig {
-	var envCfg ServerEnvConfig
+	var raw ServerEnvConfig
 
-	address := flag.String("a", "localhost:8080", "адрес сервера")
-	storeInterval := flag.Int("i", 300, "интервал времени в секундах, по истечении которого текущие показания сервера сохраняются на диск")
-	fileStoragePath := flag.String("f", "/tmp/log.json", "путь до файла, куда сохраняются текущие значения")
-	restore := flag.Bool("r", true, "определяет, загружать ли ранее сохранённые значения из указанного файла при старте сервера.")
-
+	flag.StringVar(&raw.Address, "a", "localhost:8080", "адрес сервера")
+	flag.IntVar(&raw.StoreInterval, "i", 300, "интервал времени в секундах, по истечении которого текущие показания сервера сохраняются на диск")
+	flag.StringVar(&raw.FileStoragePath, "f", "/tmp/log.json", "путь до файла, куда сохраняются текущие значения")
+	flag.BoolVar(&raw.Restore, "r", true, "определяет, загружать ли ранее сохранённые значения из указанного файла при старте сервера.")
 	flag.Parse()
 
-	err := env.Parse(&envCfg)
-	if err != nil {
+	if err := env.Parse(&raw); err != nil {
 		log.Fatalf("Ошибка конфигурации: %v", err)
 	}
 
-	if envCfg.Address != nil {
-		address = envCfg.Address
-	}
-	if envCfg.FileStoragePath != nil {
-		fileStoragePath = envCfg.FileStoragePath
-	}
-	if envCfg.StoreInterval != nil {
-		storeInterval = envCfg.StoreInterval
-	}
-	if envCfg.Restore != nil {
-		restore = envCfg.Restore
-	}
-
 	return ServerConfig{
-		Address:         *address,
-		FileStoragePath: *fileStoragePath,
-		StoreInterval:   time.Duration(*storeInterval) * time.Second,
-		Restore:         *restore,
+		Address:         raw.Address,
+		FileStoragePath: raw.FileStoragePath,
+		StoreInterval:   time.Duration(raw.StoreInterval) * time.Second,
+		Restore:         raw.Restore,
 	}
 }
